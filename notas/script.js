dba0736c595e03228f3547b62329e23436ce1e49
SuperIkerYT <ikerfernandezmarrero@gmail.com>
2025-04-07 20:33:39 +0100

    cosos para crear cosos

--- notas/script.js
+++ notas/script.js
@@ -2,7 +2,7 @@ const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
 $('.add').addEventListener("click", function (e) {
-    $('.new').style.display='flex';
+    $('.new').style.display='grid';
 })
 
 

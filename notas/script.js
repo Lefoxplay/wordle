8206861419686d4d5e76f03356bcec8e7ee6dd16
SuperIkerYT <ikerfernandezmarrero@gmail.com>
2025-04-14 20:32:45 +0100

    cosas bonitas

--- notas/script.js
+++ notas/script.js
@@ -1,8 +1,34 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
+let NotesDiv = $('#listNotes');
+
+function addElement() {
+    let newNote = document.createElement('div');
+    newNote.id = `Note ${+1}`;
+    let jinfo = infoNote();
+    newNote.innerText = jinfo.title;
+    return newNote;
+}
+
+function infoNote() {
+    let newTitle = $('#newTitle').value;
+    let newDesc = $('#newDesc').value;
+    let jinfo = {
+        title: newTitle, 
+        desc: newDesc
+    }
+    console.log(jinfo)
+    return jinfo;
+}
+
 $('.add').addEventListener("click", function (e) {
     $('.new').style.display='grid';
 })
 
+$('.create').addEventListener("click", function (e) {
+    $('.new').style.display='none';
+    NotesDiv.appendChild(addElement());
+
+})
 
